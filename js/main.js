00b7fbeacd182e982eed53c6fad4963a0fe4834f
cryptorob68 <35237756+cryptorob68@users.noreply.github.com>
2024-12-06 12:16:13 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,11 @@
 window.onload = () => {
-    const debugDiv = document.createElement('div');
-    debugDiv.classList.add('debug-info');
-    document.body.appendChild(debugDiv);
-
-    // Debugging GPS coordinates
     setInterval(() => {
         const gpsCamera = document.querySelector('[gps-camera]');
         if (gpsCamera && gpsCamera.components['gps-camera']) {
             const coords = gpsCamera.components['gps-camera'].currentCoords || {};
-            debugDiv.innerHTML = `
-                <p>Device Latitude: ${coords.latitude || 'N/A'}</p>
-                <p>Device Longitude: ${coords.longitude || 'N/A'}</p>
-                <p>Anchor Latitude: 38.4007059834491</p>
-                <p>Anchor Longitude: -121.47344204937477</p>
-            `;
+            console.log(`Device Latitude: ${coords.latitude}, Longitude: ${coords.longitude}`);
         } else {
-            debugDiv.innerHTML = `<p>Waiting for GPS data...</p>`;
+            console.log('Waiting for GPS data...');
         }
     }, 1000);
 };
